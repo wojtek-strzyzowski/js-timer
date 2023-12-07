9d07a62219768982e0c4f9b630127998a4c420d7
wojtek-strzyzowski <151506991+wojtek-strzyzowski@users.noreply.github.com>
2023-12-07 16:39:43 +0100

    Add files via upload

--- timer.js
+++ timer.js
@@ -1,94 +1,72 @@
 
-let countdownTime
-//let countDownDate = new Date().getTime();
-// Update the count down every 1 second
-let interval = setInterval(countDown, 1000);
+let countdownTime;
+let interval;
+let distance = 0;
+
+// grab the cards and save in variables
+let daysCard = document.getElementById("daysCard");
+let hoursCard = document.getElementById("hoursCard");
+let minutesCard = document.getElementById("minutesCard");
+let secondsCard = document.getElementById("secondsCard");
 
 
 function setDate() {
   event.preventDefault()
   countdownTime = new Date(document.getElementById("countdownTime").value).getTime();
-  interval
-}
-
-
-
-function countDown() {
-
-  // Get today's date and time
-  let now = new Date().getTime();
-    
-  // Find the distance between now and the count down date
-  let distance = countdownTime - now;
-    
-  let days = 0;
-  let hours = 0;
-  let minutes = 0;
-  let seconds = 0;
-  // Time calculations for days, hours, minutes and seconds
-  if(countdownTime !== NaN && countdownTime !== undefined) {
-    days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  } 
-  if (distance < 0) {
-    alert ("Die angegebene Zeit liegt in der Vergangenheit!! Du depp!!")
+  
+  // Validation der Datumseingabe
+  if((countdownTime - (new Date().getTime())) < 0){
+    clearInterval(interval);
+    alert ("Die angegebene Zeit liegt in der Vergangenheit!! Du depp!!");
+    distance = 0;
     location.reload();
   }
-  
- 
+  interval = setInterval(countDown, 1000);
+}
 
 
-  // Update the "Tage"-Card with the remaining days
-  document.getElementById("daysCard").innerHTML = days + " days";
+// wird durch das intervall jede Sekunde ausgeführt
+function countDown() {
 
-  //update Stunden-Card with remaining hours
-  document.getElementById("hoursCard").innerHTML = hours + " Hours";
-
-  //update Minuten-Card with remaining Minutes
-  document.getElementById("minutesCard").innerHTML = minutes + " Minutes";
-
-  //update Seconds-Card with remaining Seconds
-  document.getElementById("secondsCard").innerHTML = seconds + " Seconds";
+  // Get miliseconds of todays date
+  let now = new Date().getTime();
+  console.log('countdown: ' + countdownTime);
     
-  // If the count down is over, write some text 
+  // Find the distance between now and the count down date
+  distance = countdownTime - now;
+  console.log('Übrige Zeit: ' + distance);
+
+  // Unterbreche den Countdown, wenn Countdown kleiner als 0
   if (distance <= 0) {
     clearInterval(interval);
-    document.getElementById("timer").innerHTML = "Zeit ist abgelaufen";
+    distance = 0;
+    alert ("Die Zeit ist abgelaufen.");
   }
-}
+    
 
-let timer;
-let hoursInput = document.getElementById("hours");
-let minutesInput = document.getElementById("minutes");
-let secondsInput = document.getElementById("seconds");
-let timerDisplay = document.getElementById("timer");
+  // Time calculations for days, hours, minutes and seconds
+  if(!isNaN(distance)) {
 
-function startTimer() {
-    let hours = parseInt(hoursInput.value) || 0;
-    let minutes = parseInt(minutesInput.value) || 0;
-    let seconds = parseInt(secondsInput.value) || 0;
+     // Update the "Tage"-Card with the remaining days
+     daysCard.innerHTML = Math.floor(distance / (1000 * 60 * 60 * 24)) + " days";
 
-    let totalSeconds = hours * 3600 + minutes * 60 + seconds;
+     //update Stunden-Card with remaining hours
+     hoursCard.innerHTML = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) + " Hours";
+   
+     //update Minuten-Card with remaining Minutes
+     minutesCard.innerHTML = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)) + " Minutes";
+   
+     //update Seconds-Card with remaining Seconds
+     secondsCard.innerHTML = Math.floor((distance % (1000 * 60)) / 1000) + " Seconds";
+   
+  }
+  
+  else {
+    clearInterval(interval);
+    alert ("Falsche Zeitangabe");
+    distance = 0;
+    location.reload();
+  }
+    
 
-    if (totalSeconds > 0) {
-        timer = setInterval(function() {
-            if (totalSeconds <= 0) {
-                clearInterval(timer);
-                timerDisplay.innerHTML = "00:00:00";
-            } else {
-                let hours = Math.floor(totalSeconds / 3600);
-                let minutes = Math.floor((totalSeconds % 3600) / 60);
-                let seconds = totalSeconds % 60;
-
-                timerDisplay.innerHTML = formatTime(hours) + ":" + formatTime(minutes) + ":" + formatTime(seconds);
-                totalSeconds--;
-            }
-        }, 1000);
-    }
-}
-
-function pauseTimer() {
-    clearInterval(timer);
 }
